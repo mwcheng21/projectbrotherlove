33ed51f7ed9e466aca6c6beb2a43b0821a24ecd4
Micah Cheng <mwcheng@umich.edu>
2020-12-17 17:53:29 -0500

    Add social links

--- assets/js/my.js
+++ assets/js/my.js
@@ -2,3 +2,4 @@ function showDetails(eventtime, eventname, pagename) {
     $('#schedule-content').load(pagename + '.html')
     $('#schedule-name').html(eventtime + " - <strong>" + eventname + "</strong>")
 }
+
