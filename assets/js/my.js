d280e8612b93f29cd844657e3c329c9368265548
Micah Cheng <mwcheng@umich.edu>
2020-12-17 21:44:55 -0500

    Add sticky nav

--- assets/js/my.js
+++ assets/js/my.js
@@ -3,3 +3,17 @@ function showDetails(eventtime, eventname, pagename) {
     $('#schedule-name').html(eventtime + " - <strong>" + eventname + "</strong>")
 }
 
+//color sticky nav
+window.onscroll = function() {colorNav()};
+$("#nav").css("position", "fixed")
+function colorNav() {
+  if (window.pageYOffset != 0) {
+      $("#nav").css("color", "black")
+      $("#nav").css("background-color", "#f0f4f4")
+    //navbar.classList.add("sticky")
+  } else {
+    //navbar.classList.remove("sticky");
+    $("#nav").css("color", "white")
+    $("#nav").css("background-color", "transparent ")
+}
+}
