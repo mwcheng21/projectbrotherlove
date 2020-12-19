049dfc76b3faca1fa6d5596b9ca66adca59ec8c2
Micah Cheng <mwcheng@umich.edu>
2020-12-18 20:34:49 -0500

    lazy load images

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,20 +1,15 @@
-function showDetails(eventtime, eventname, pagename) {
-  $('#schedule-content').load(pagename + '.html')
-  $('#schedule-name').html(eventtime + " - <strong>" + eventname + "</strong>")
+function showDetails(eventtime, eventname, pagename) {     $('#schedule-content').load(pagename + '.html')
+    $('#schedule-name').html(eventtime + " - <strong>" + eventname + "</strong>")
 }
 
 //color sticky nav
-if (!browser.mobile) {
-  //color sticky nav
-  window.onscroll = function () { colorNav() };
-  $("#nav").css("position", "fixed")
-  $("#nav").css("z-index", "2")
-}
+window.onscroll = function() {colorNav()};
+$("#nav").css("position", "fixed")
+$("#nav").css("z-index", "2")
 function colorNav() {
   if (window.pageYOffset != 0) {
     document.getElementById("nav").classList.add("darkened")
   } else {
     document.getElementById("nav").classList.remove("darkened");
-
   }
 }
