deb51e5fb247ff63994710bef0d90b76b18da5cf
Micah Cheng <heymicahme@gmail.com>
2020-12-19 00:11:22 +0000

    Fix mobile nav bar

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,17 +1,20 @@
 function showDetails(eventtime, eventname, pagename) {
-    $('#schedule-content').load(pagename + '.html')
-    $('#schedule-name').html(eventtime + " - <strong>" + eventname + "</strong>")
+  $('#schedule-content').load(pagename + '.html')
+  $('#schedule-name').html(eventtime + " - <strong>" + eventname + "</strong>")
 }
 
 //color sticky nav
-window.onscroll = function() {colorNav()};
-$("#nav").css("position", "fixed")
-$("#nav").css("z-index", "2")
+if (!browser.mobile) {
+  //color sticky nav
+  window.onscroll = function () { colorNav() };
+  $("#nav").css("position", "fixed")
+  $("#nav").css("z-index", "2")
+}
 function colorNav() {
   if (window.pageYOffset != 0) {
     document.getElementById("nav").classList.add("darkened")
   } else {
     document.getElementById("nav").classList.remove("darkened");
 
-}
+  }
 }
