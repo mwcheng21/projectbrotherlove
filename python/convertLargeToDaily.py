b730f40ad56bc81bd83b76ec94895c5c8ce91b2a
Micah Cheng <heymicahme@gmail.com>
2021-08-08 15:35:09 -0400

    Update wed schedule

--- python/convertLargeToDaily.py
+++ python/convertLargeToDaily.py
@@ -13,7 +13,9 @@ def getData(keypair):
 	return keypair[keypair.find('="')+2:][:-1]
 def convertTimeToAmPm(time):
 	hr, min = time.split(":")
-	if (int(hr) < 9 or int(hr) == 12):
+	if (int(hr) < 8 or int(hr) >= 12):
+		if (int(hr) > 12):
+			return str(int(hr) - 12) + "pm"
 		return time + "pm"
 	return time + "am"
 outputString = ""
@@ -34,7 +36,7 @@ with open('largeSchedule.txt') as f:
 		if (startNewEvent):
 			if (eventLine == 1):
 				info = line.split()
-				tmpOutput = eventDiv.replace("{{startTime}}", getData(info[1])).replace("{{endTime}}", getData(info[2]))
+				tmpOutput = eventDiv.replace("{{startTime}}", convertTimeToAmPm(getData(info[1]))).replace("{{endTime}}", convertTimeToAmPm(getData(info[2])))
 				tmpOutput = tmpOutput.replace("{{eventNickname}}", getData(info[3]))
 			elif (eventLine == 2):
 				tmpOutput = tmpOutput.replace("{{eventName}}", line[line.find('">')+2:])	
