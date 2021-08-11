4e5b2a0e9ab122a0f3185647221f6807f5e71202
repl.it user <replituser@example.com>
2021-08-11 02:20:41 +0000

    Merge counselors

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,4 +1,5 @@
-function showDetails(eventtime, eventname, pagename) {     $('#schedule-content').load(pagename + '.html')
+function showDetails(eventtime, eventname, pagename) {      $('#schedule-content').html("") 
+  $('#schedule-content').load(pagename + '.html')
     $('#schedule-name').html(eventtime + " - <strong>" + eventname + "</strong>")
 }
 
